46aa6ba158642924edc44e091ba1df54d90d721f
Azhar Uddin <63742495+Azhar-1234@users.noreply.github.com>
2023-05-03 21:50:48 +0600

    Update loop.php

--- loop.php
+++ loop.php
@@ -1,7 +1,7 @@
 <?php
-	$array=['a','b','c','d','e'];
+// 	$array=['a','b','c','d','e'];
 	
-	$count = sizeof($array);
+// 	$count = sizeof($array);
 	// $i=0;
 	// while ( $i <$count) {
 	// 	echo $array[$i];
@@ -11,6 +11,22 @@
 	// for ($i=0; $i < $count ; $i++) { 
 	// 	echo $array[$i];
 	// }
+	foreach ($object as $property => $value) {
+    	  // Do something with $property and $value
+        }
+
+	class Person {
+	    public $name = "John";
+	    public $age = 30;
+	    public $city = "New York";
+	}
+
+	$person = new Person();
+
+	foreach ($person as $property => $value) {
+	    echo "$property: $value<br>";
+	}
+
 
 
 
